72c911e2ac2b78695be612cd3b7d9f51d9e12b50
RLGHISLENI <beto.ghisleni@gmail.com>
2016-05-19 17:23:37 -0300

    Add Inicial

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ExtensionMethods{
+  public class Pessoa
+  {
+    public string Nome { get; set; }
+    public string Sobrenome { get; set; }
+    public string FormatarNome()
+    {
+      return Nome + " " + Sobrenome; 
+    }
+
+  }
+
+  public static class ExtendPessoa
+  {
+    public static int NomeLength(this Pessoa pessoa)
+    {
+      return pessoa.FormatarNome().Length;
+    }
+  }
+ 
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var pessoa = new Pessoa()
+      {
+        Nome = "Roberto",
+        Sobrenome = "Ghisleni"
+      };
+
+      Console.WriteLine(pessoa.NomeLength());
+      
+    }
+  }
+}
